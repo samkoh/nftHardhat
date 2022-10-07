d29c0be2ca3dde60facf538be5e7309304964f69
Sam Koh <sam910615@gmail.com>
2022-10-07 20:16:50 +0800

    added .env

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,18 +1,19 @@
 
 require("@nomiclabs/hardhat-waffle");
 
+require('dotenv').config();
+
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
-
-const Private_Key = "28e070d527afe9d293079b864aaaebcffec52b060a458a269fbe7e87eab13126"
 
 module.exports = {
   solidity: "0.8.4",
   networks: {
     goerli: {
-      url: `https://eth-goerli.g.alchemy.com/v2/I_miv0scgsWRoSmqci2kEupz9mHoIfOs`,
-      accounts: [`0x${Private_Key}`]
+      // url: `https://eth-goerli.g.alchemy.com/v2/I_miv0scgsWRoSmqci2kEupz9mHoIfOs`,
+      url: `https://eth-goerli.g.alchemy.com/v2/${process.env.INFURA_PROJECT_ID}`,
+      accounts: [`0x` + process.env.PRIVATE_KEY],
     }
   }
 };
