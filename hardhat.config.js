93cb7d92927ac3873139745760da843c5ceee1f9
Sam Koh <sam910615@gmail.com>
2022-10-07 20:30:52 +0800

    update config

--- hardhat.config.js
+++ hardhat.config.js
@@ -11,7 +11,6 @@ module.exports = {
   solidity: "0.8.4",
   networks: {
     goerli: {
-      // url: `https://eth-goerli.g.alchemy.com/v2/I_miv0scgsWRoSmqci2kEupz9mHoIfOs`,
       url: `https://eth-goerli.g.alchemy.com/v2/${process.env.INFURA_PROJECT_ID}`,
       accounts: [`0x` + process.env.PRIVATE_KEY],
     }
