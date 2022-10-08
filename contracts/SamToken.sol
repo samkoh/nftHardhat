e6538fef68a585dcdb69490819bdbec2c04f2424
Sam Koh <sam910615@gmail.com>
2022-10-08 11:38:03 +0800

    add contract verification

--- contracts/SamToken.sol
+++ contracts/SamToken.sol
@@ -3,25 +3,26 @@ pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract SamToken is ERC721, Pausable, ERC721Burnable {
+contract SamToken is ERC721, Pausable, Ownable, ERC721Burnable {
     using Counters for Counters.Counter;
 
     Counters.Counter private _tokenIdCounter;
 
     constructor() ERC721("SamToken", "STK") {}
 
-    function pause() public {
+    function pause() public onlyOwner {
         _pause();
     }
 
-    function unpause() public {
+    function unpause() public onlyOwner {
         _unpause();
     }
 
-    function safeMint(address to) public {
+    function safeMint(address to) public whenNotPaused {
         uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
         _safeMint(to, tokenId);
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,6 @@
 
 require("@nomiclabs/hardhat-waffle");
-
+require("@nomiclabs/hardhat-etherscan");
 require('dotenv').config();
 
 /**
@@ -11,8 +11,14 @@ module.exports = {
   solidity: "0.8.4",
   networks: {
     goerli: {
-      url: `https://eth-goerli.g.alchemy.com/v2/${process.env.INFURA_PROJECT_ID}`,
-      accounts: [`0x` + process.env.PRIVATE_KEY],
+      url: process.env.GOERLI_PROJECT_ID,
+      // url: `https://eth-goerli.g.alchemy.com/v2/I_miv0scgsWRoSmqci2kEupz9mHoIfOs`,
+      accounts: [process.env.PRIVATE_KEY],
+      // accounts: [`0x28e070d527afe9d293079b864aaaebcffec52b060a458a269fbe7e87eab13126`]
     }
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_TOKEN
+    // apiKey: "NF2NF8B964M8YA5P8P1R25K9TPBHI9MQWR"
   }
 };
