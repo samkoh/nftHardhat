f613bafba8a4ec96dee40658f6affb3975fc17ea
Sam Koh <sam910615@gmail.com>
2022-10-08 11:40:09 +0800

    update hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -12,13 +12,10 @@ module.exports = {
   networks: {
     goerli: {
       url: process.env.GOERLI_PROJECT_ID,
-      // url: `https://eth-goerli.g.alchemy.com/v2/I_miv0scgsWRoSmqci2kEupz9mHoIfOs`,
       accounts: [process.env.PRIVATE_KEY],
-      // accounts: [`0x28e070d527afe9d293079b864aaaebcffec52b060a458a269fbe7e87eab13126`]
     }
   },
   etherscan: {
     apiKey: process.env.ETHERSCAN_API_TOKEN
-    // apiKey: "NF2NF8B964M8YA5P8P1R25K9TPBHI9MQWR"
   }
 };
